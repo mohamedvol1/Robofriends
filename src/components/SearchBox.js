61f24a460e892a191d798cc0701e43a8fe8a3c9c
mohamedvol1 <70990780+mohamedvol1@users.noreply.github.com>
2021-08-27 19:05:51 +0200

    add a simple style and some margins

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchChange }) => {
   return (
     <input
-      className='pa3 ba b--green bg-lightest-blue' 
+      className='pa3 ba b--green bg-lightest-blue mb3' 
       type='search' 
       placeholder='search robots'
       onChange={searchChange} 
